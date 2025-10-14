95f4ff416001aa76cde6cc09a378dc81fc39a994
Syakirin Amin <akrinmin@gmail.com>
2025-10-14 13:06:39 +0700

    fix: Remove translation functions for static text in user creation form

--- resources/views/livewire/users/create.blade.php
+++ resources/views/livewire/users/create.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-slot:pageActions>
         <a href="{{ route('users.index') }}" class="btn-outline-secondary btn">
-            {{ __('Kembali ke daftar pengguna') }}
+            Kembali ke daftar pengguna
         </a>
     </x-slot:pageActions>
 
@@ -10,7 +10,7 @@
             <div class="row g-4">
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label class="form-label" for="create-name">{{ __('Nama') }}</label>
+                        <label class="form-label" for="create-name">Nama</label>
                         <input
                             id="create-name"
                             type="text"
@@ -27,7 +27,7 @@
 
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label class="form-label" for="create-email">{{ __('Alamat email') }}</label>
+                        <label class="form-label" for="create-email">Alamat email</label>
                         <input
                             id="create-email"
                             type="email"
@@ -44,7 +44,7 @@
 
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label class="form-label" for="create-password">{{ __('Kata sandi') }}</label>
+                        <label class="form-label" for="create-password">Kata sandi</label>
                         <input
                             id="create-password"
                             type="password"
@@ -61,7 +61,7 @@
 
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label class="form-label" for="create-password-confirmation">{{ __('Konfirmasi kata sandi') }}</label>
+                        <label class="form-label" for="create-password-confirmation">Konfirmasi kata sandi</label>
                         <input
                             id="create-password-confirmation"
                             type="password"
@@ -78,18 +78,18 @@
 
                 <div class="col-12">
                     <hr class="my-4">
-                    <h3 class="mb-4">{{ __('Informasi Identitas') }}</h3>
+                    <h3 class="mb-4">Informasi Identitas</h3>
                 </div>
 
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label class="form-label" for="identity-id">{{ __('ID Identitas') }}</label>
+                        <label class="form-label" for="identity-id">ID Identitas</label>
                         <input
                             id="identity-id"
                             type="text"
                             class="form-control @error('identity_id') is-invalid @enderror"
                             wire:model.defer="identity_id"
-                            placeholder="{{ __('mis., NIP, NIDN, atau ID Karyawan') }}"
+                            placeholder="mis., NIP, NIDN, atau ID Karyawan"
                             required
                         >
                         @error('identity_id')
@@ -100,7 +100,7 @@
 
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label class="form-label" for="birthplace">{{ __('Tempat Lahir') }}</label>
+                        <label class="form-label" for="birthplace">Tempat Lahir</label>
                         <input
                             id="birthplace"
                             type="text"
@@ -115,7 +115,7 @@
 
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label class="form-label" for="birthdate">{{ __('Tanggal Lahir') }}</label>
+                        <label class="form-label" for="birthdate">Tanggal Lahir</label>
                         <input
                             id="birthdate"
                             type="date"
@@ -130,7 +130,7 @@
 
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label class="form-label" for="address">{{ __('Alamat') }}</label>
+                        <label class="form-label" for="address">Alamat</label>
                         <textarea
                             id="address"
                             class="form-control @error('address') is-invalid @enderror"
@@ -145,7 +145,7 @@
 
                 <div class="col-12">
                     <div class="mb-3">
-                        <label class="form-label">{{ __('Tetapkan peran') }}</label>
+                        <label class="form-label">Tetapkan peran</label>
                         <div class="form-selectgroup @error('selectedRole') is-invalid @enderror">
                             @foreach ($roleOptions as $option)
                                 <label class="form-selectgroup-item">
@@ -169,7 +169,7 @@
                         @error('selectedRole')
                             <div class="d-block invalid-feedback">{{ $message }}</div>
                         @enderror
-                        <p class="mt-2 text-secondary">{{ __('Biarkan tidak dipilih untuk menetapkan peran nanti.') }}</p>
+                        <p class="mt-2 text-secondary">Biarkan tidak dipilih untuk menetapkan peran nanti.</p>
                     </div>
                 </div>
             </div>
@@ -177,11 +177,11 @@
 
         <div class="d-flex align-items-center justify-content-between card-footer">
             <a href="{{ route('users.index') }}" class="btn btn-link">
-                {{ __('Batal') }}
+                Batal
             </a>
             <button type="submit" class="btn btn-primary" wire:loading.attr="disabled">
-                <span wire:loading.remove>{{ __('Buat pengguna') }}</span>
-                <span wire:loading>{{ __('Membuat...') }}</span>
+                <span wire:loading.remove>Buat pengguna</span>
+                <span wire:loading>Membuat...</span>
             </button>
         </div>
     </form>
