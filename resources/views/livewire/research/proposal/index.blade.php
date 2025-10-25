b773d0be22791228778c582f6a8ebb05a4e28d11
Syakirin Amin <akrinmin@gmail.com>
2025-10-25 17:40:01 +0700

    feat: Add proposal management UI components to the research proposal page

--- resources/views/livewire/research/proposal/index.blade.php
+++ resources/views/livewire/research/proposal/index.blade.php
@@ -1,3 +1,20 @@
+<x-slot:title>Usulan Proposal</x-slot:title>
+<x-slot:pageSubtitle>Kelola usulan proposal penelitian Anda</x-slot:pageSubtitle>
+<x-slot:pageTitle>Usulan Proposal</x-slot:pageTitle>
+<x-slot:pageActions>
+    <a href="#" class="btn btn-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon">
+            <path d="M12 5l0 14"></path>
+            <path d="M5 12l14 0"></path>
+        </svg>
+        Tambah Usulan
+    </a>
+</x-slot:pageActions>
+
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <div class="card">
+        <div class="card-body">
+            <p>Proposals content goes here...</p>
+        </div>
+    </div>
 </div>
