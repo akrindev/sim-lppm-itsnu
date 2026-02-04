250a1e65ff0fcd34351b9b86704cfffda621135d
Syakirin Amin <akrinmin@gmail.com>
2026-02-04 15:03:22 +0700

    feat(installer): add FakeInstallationService for testing and streamline installation process in tests

--- tests/Feature/Installer/InstallerWizardTest.php
+++ tests/Feature/Installer/InstallerWizardTest.php
@@ -6,62 +6,7 @@ use App\Livewire\Installer\InstallerWizard;
 use App\Services\Installer\InstallationService;
 use Illuminate\Support\Str;
 use Livewire\Livewire;
-
-class FakeInstallationService extends InstallationService
-{
-    public array $lastConfig = [];
-
-    private bool $running = false;
-
-    private array $storedConfig = [];
-
-    public function checkEnvironment(): array
-    {
-        return [];
-    }
-
-    public function allEnvironmentChecksPass(): bool
-    {
-        return true;
-    }
-
-    public function runInstallation(array $config, callable $onProgress, ?callable $onStep = null): void
-    {
-        $this->lastConfig = $config;
-
-        $onProgress(100, 'Installation complete!');
-    }
-
-    public function storeInstallationConfig(array $config): void
-    {
-        $this->storedConfig = $config;
-    }
-
-    public function getInstallationConfig(): array
-    {
-        return $this->storedConfig;
-    }
-
-    public function clearInstallationConfig(): void
-    {
-        $this->storedConfig = [];
-    }
-
-    public function isInstallationRunning(): bool
-    {
-        return $this->running;
-    }
-
-    public function markInstallationRunning(): void
-    {
-        $this->running = true;
-    }
-
-    public function markInstallationStopped(): void
-    {
-        $this->running = false;
-    }
-}
+use Tests\Support\FakeInstallationService;
 
 beforeEach(function () {
     config(['app.key' => 'base64:'.base64_encode(Str::random(32))]);
