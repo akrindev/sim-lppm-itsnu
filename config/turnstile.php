bf8c970919308bebe7e6cfd44e3bb04db5c63093
Syakirin Amin <akrinmin@gmail.com>
2026-02-19 01:20:52 +0700

    chore: disable turnstile on local development

--- config/turnstile.php
+++ config/turnstile.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'site_key' => env('TURNSTILE_SITE_KEY'),
-    'secret_key' => env('TURNSTILE_SECRET_KEY'),
+    'site_key' => app()->isLocal() ? null : env('TURNSTILE_SITE_KEY'),
+    'secret_key' => app()->isLocal() ? null : env('TURNSTILE_SECRET_KEY'),
 ];
