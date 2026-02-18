765f6dfbccf4f59eeba0f7a47020593a015850b7
Syakirin Amin <akrinmin@gmail.com>
2026-02-19 01:50:21 +0700

    fix: avoid container lookup during turnstile config bootstrap

--- config/turnstile.php
+++ config/turnstile.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'site_key' => app()->isLocal() ? null : env('TURNSTILE_SITE_KEY'),
-    'secret_key' => app()->isLocal() ? null : env('TURNSTILE_SECRET_KEY'),
+    'site_key' => env('APP_ENV') === 'local' ? null : env('TURNSTILE_SITE_KEY'),
+    'secret_key' => env('APP_ENV') === 'local' ? null : env('TURNSTILE_SECRET_KEY'),
 ];
